ce7244d8f66c2d55ae268eece6bfd96bad1d2997
Michał Jaworek <jaworekmichal@gmail.com>
2013-01-11 13:08:32 +0100

    Podstawa mapy (klasa Mapa)

--- src/pl/edu/pk/kni/mobile/wifihertz/EkranPomiaru.java
+++ src/pl/edu/pk/kni/mobile/wifihertz/EkranPomiaru.java
@@ -1,16 +1,41 @@
 package pl.edu.pk.kni.mobile.wifihertz;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
 public class EkranPomiaru extends Activity {
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		mapa.pause();
+	}
+
+
+
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		mapa.resume();
+	}
+
 	WifiManager wifi;
+	private Mapa mapa;
+	float polozenieMapyX;
+	float polozenieMapyY;
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ekran_pomiaru);
+        mapa = new Mapa(this);
+        setContentView(mapa);
     }
 
   
@@ -22,4 +47,53 @@ public class EkranPomiaru extends Activity {
     
         return true;
     }
+    
+    class Mapa extends SurfaceView implements Runnable{
+    	Thread t = null;
+    	SurfaceHolder holder;
+    	Boolean dzialaj = false;
+		
+    	public Mapa(Context context){
+    		super(context);
+    		holder = getHolder();
+    	}
+    	
+    	public void run() {
+			while(dzialaj)
+			{
+				if(!holder.getSurface().isValid())
+					continue;
+				
+				Canvas c = holder.lockCanvas();
+				c.drawARGB(255, 255, 255, 200);
+				holder.unlockCanvasAndPost(c);	
+				
+			}
+			
+		}
+		
+		public void resume(){
+			dzialaj = true;
+			t = new Thread(this);
+			t.start();
+		}
+		
+		public void pause(){
+			dzialaj = false;
+			while(true)
+			{
+				try {
+					t.join();
+				} catch (InterruptedException e) {
+				
+					e.printStackTrace();
+				}
+				break;
+			}
+			t = null;
+		}
+    	
+    	
+    }
+    
 }
